364f4feddfac2eb4bc8eba3bb74ccff1dc58bdfa
msgal97 <klyoung2@mail.bradley.edu>
2015-12-09 19:28:27 -0600

    added css

--- final.php
+++ final.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <style>
+<link rel="stylesheet" type="text/css" href="final.css">
 .error {color: #FF0000;}
 </style>
 </head>
@@ -12,42 +13,6 @@
 $nameErr = $emailErr = $fateErr = $websiteErr = "";
 $name = $email = $fate = $comment = $website = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-   if (empty($_POST["name"])) {
-     $nameErr = "Name is required";
-   } else {
-     $name = test_input($_POST["name"]);
-     // check if name only contains letters and whitespace
-     if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
-       $nameErr = "Only letters and white space allowed";
-     }
-   }
-
-   if (empty($_POST["email"])) {
-     $emailErr = "Email is required";
-   } else {
-     $email = test_input($_POST["email"]);
-     // check if e-mail address is well-formed
-     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-       $emailErr = "Invalid email format";
-     }
-   }
-
-   if (empty($_POST["website"])) {
-     $website = "";
-   } else {
-     $website = test_input($_POST["website"]);
-     // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
-     if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
-       $websiteErr = "Invalid URL";
-     }
-   }
-
-   if (empty($_POST["comment"])) {
-     $comment = "";
-   } else {
-     $comment = test_input($_POST["comment"]);
-   }
 
    if (empty($_POST["fate"])) {
      $fateErr = "fate is required";
